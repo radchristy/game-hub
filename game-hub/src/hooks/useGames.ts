afae9e90ec60d182c6c52ad49307f0af9572fe6c
Christine Hong <christinehong003@gmail.com>
2024-10-18 14:59:09 -0400

    Displaying platform icons

--- game-hub/src/hooks/useGames.ts
+++ game-hub/src/hooks/useGames.ts
@@ -3,10 +3,17 @@ import apiClient from "../services/api-client";
 import { CANCELLED } from "dns";
 import { CanceledError } from "axios";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
+
 export interface Game {
   id: number;
   name: string;
   background_image: string;
+  parent_platforms: { platform: Platform }[];
 }
 
 interface FetchGamesResponse {
