faa5be7b029d4570a65ec422e2e1301d83557743
Christine Hong <christinehong003@gmail.com>
2025-01-16 15:46:46 -0500

    React Query for fetching platforms & fixed playstation platform issue

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,11 +1,19 @@
-import useData from "./useData";
+import { useQuery } from "@tanstack/react-query";
+import apiClient from "../services/api-client";
+import platforms from "../data/platforms";
+import { FetchResponse } from "./useData";
+import { Platform } from "./useGames";
 
-interface Platform {
-  id: number;
-  name: string;
-  slug: string;
-}
+const usePlatforms = () =>
+  useQuery({
+    queryKey: ["platforms"],
+    queryFn: () =>
+      apiClient
+        .get<FetchResponse<Platform>>("/platforms/lists/parents")
 
-const usePlatforms = () => useData<Platform>("platforms");
+        .then((res) => res.data),
+    staleTime: 24 * 60 * 60 * 1000, //24h
+    initialData: { count: platforms.length, results: platforms },
+  });
 
 export default usePlatforms;
