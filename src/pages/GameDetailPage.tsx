e2811fb10222d5b610593e3ef7557f0966540388
Christine Hong <christinehong003@gmail.com>
2025-01-22 12:03:39 -0500

    Build expandable text

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,5 +1,6 @@
 import { Heading, Spinner, Text } from "@chakra-ui/react";
 import { useParams } from "react-router-dom";
+import ExpandableText from "../components/ExpandableText";
 import useGame from "../hooks/useGame";
 
 const GameDetailPage = () => {
@@ -10,8 +11,8 @@ const GameDetailPage = () => {
   if (error || !game) throw error;
   return (
     <>
-          <Heading>{game.name}</Heading>
-          <Text>{ game.description_raw}</Text>
+      <Heading>{game.name}</Heading>
+      <ExpandableText>{game.description_raw}</ExpandableText>
     </>
   );
 };
